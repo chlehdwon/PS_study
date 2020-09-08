c062f53805b519ec67730f57b31981bcdef4477b
chlehdwon <cookie000215@gmail.com>
2020-09-08 15:01:15 +0000

    Problem 7 Solved (2020-09-09)

--- 7.py
+++ 7.py
@@ -3,8 +3,31 @@
 
 class Solution:
     def reverse(self, x):
-        pass
+        max = (1 << 31) - 1
+        min = ~(1 << 31) + 1
+        if x > 0:
+            a = int(str(x)[::-1])
+            if a > max:
+                return 0
+            else:
+                return a
+        else:
+            b = -int(str(-x)[::-1])
+            if b < min:
+                return 0
+            else:
+                return b
+
+    def reverse_f(self, x):
+        s = -1 if (x < 0) else 1
+        num = int(str(abs(x))[::-1])
+        if (num > -2147483648 and num < 2147483647):
+            return num*s
+        else:
+            return 0
 
 
 a = Solution()
-print(a.reverse(123))
+print(a.reverse(1534236469))
+print(a.reverse(-312))
+
