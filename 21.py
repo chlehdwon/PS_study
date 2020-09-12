15e5d71c16020edd92714d8d7454aed1bad35e98
chlehdwon <cookie000215@gmail.com>
2020-09-12 06:48:38 +0000

    Problem 5, 21 Solved (2020-09-12)

--- 21.py
+++ 21.py
@@ -1,3 +1,6 @@
+# Merge two sorted linked lists and return it as a new sorted list.
+# The new list should be made by splicing together
+# the nodes of the first two lists.
 # Definition for singly-linked list.
 
 
@@ -6,30 +9,69 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
     def mergeTwoLists(self, l1, l2):
-        n_l1, n_l2, n_l3, num = l1, l2, None, 0
-        if n_l1.val >= nl2.val:
-            n_l3 = n_l2
-            num = 1
-        else:
-            n_l3 = n_l1
-            num = 0
-        while (not n_l1) and (not n_l2):
-            if num == 0:
-                if n_l1.next.val < n_l2.val:
-                    n_l3.next = n_l1.next
-                    n_l1.next = n_l1
-                else:
-                    n_13.next = n_l2
-                    n_l2.next = n_l2
-                    num = 1
+        n_l3 = l3 = ListNode()
+        while l1 or l2:
+            if not l1:
+                n_l3.next = l2
+                break
+            elif not l2:
+                n_l3.next = l1
+                break
             else:
-                if n_l2.next.val < n_l1.val:
-                    n_l3.next = n_l2.next
-                    n_l2.next = n_l2
-                    num = 0
-                else:
-                    n_13.next = n_l1
-                    n_l1.next = n_l1
-        return l3
+                if l1.val <= l2.val:
+                    n_l3.next = l1
+                    n_l3 = n_l3.next
+                    l1 = l1.next
+                elif l1.val > l2.val:
+                    n_l3.next = l2
+                    n_l3 = n_l3.next
+                    l2 = l2.next
+        return l3.next
+
+    def mergeTwoLists_2(self, l1, l2):
+        # Another solution which using recursion
+        temp = ListNode
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val < l2.val:
+            temp = l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        else:
+            temp = l2
+            l2.next = self.mergeTwoLists(l2.next, l1)
+        return temp
+
+
+a = Solution()
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(4)
+n1.next, n2.next = n2, n3
+n4 = ListNode(1)
+n5 = ListNode(3)
+n6 = ListNode(4)
+n4.next, n5.next = n5, n6
+n7 = a.mergeTwoLists_2(n1, n4)
+while n7:
+    print(n7.val)
+    n7 = n7.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 5.py
@@ -0,0 +1,20 @@
+# Given a string s, find the longest palindromic substring in s.
+# You may assume that the maximum length of s is 1000.
+
+class Solution:
+    def longestPalindrome(self, s):
+        def longestPalindromeCenteredAt(i, j):
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            return s[i + 1:j]
+        result = ''
+        for i in range(len(s)):
+            newResult = longestPalindromeCenteredAt(i, i)
+            if len(newResult) > len(result):
+                result = newResult
+            if i + 1 < len(s):
+                newResult = longestPalindromeCenteredAt(i, i + 1)
+                if len(newResult) > len(result):
+                    result = newResult
+        return result
