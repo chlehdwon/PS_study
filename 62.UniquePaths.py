9f69eea256582c3198079bfb22654e5ab494cf02
chlehdwon <cookie000215@gmail.com>
2020-10-21 14:31:12 +0000

    2020-10-21

--- 62.UniquePaths.py
+++ 62.UniquePaths.py
@@ -32,5 +32,33 @@ class Solution:
         return self.recursive_unique(m, n)
 
 
+class Solution2:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # DP Solution
+        if m == 1 or n == 1:
+            return 1
+        
+        '''
+        # O(m*n) Space Complexity 
+        dp = [[1] * n for i in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]      
+        return dp[m-1][n-1]
+        '''
+        
+        # O(n) Space Complexity 
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j-1]
+        return dp[n-1]
+
+
 a = Solution()
 print(a.uniquePaths(3, 7))
