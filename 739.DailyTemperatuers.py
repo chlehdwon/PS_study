c1af446f14da4bad5f837c6aee22903ed05e2ba4
chlehdwon <cookie000215@gmail.com>
2021-01-12 14:47:17 +0000

    2021-01-12

--- 739.DailyTemperatuers.py
+++ 739.DailyTemperatuers.py
@@ -7,11 +7,10 @@ class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
         if not T:
             return T
-        stack, day_list = [0], [0 for i in range(len(T))]
-        for i in range(1, len(T)):
-            while stack and T[stack[-1]] < T[i]:
-                for j in stack:
-                    day_list[j] += 1
-                stack.pop()
+        stack, day_list = [], [0 for i in range(len(T))]
+        for i, temp in enumerate(T):
+            while stack and T[stack[-1]] < temp:
+                day = stack.pop()
+                day_list[day] = i - day 
             stack.append(i)
         return day_list
