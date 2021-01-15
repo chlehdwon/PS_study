fec8084c431e9a99a11a77ec4c764dc8dc87f5b7
chlehdwon <cookie000215@gmail.com>
2021-01-15 14:08:41 +0000

    2021-01-15

--- 23.MergekSortedLists.py
+++ 23.MergekSortedLists.py
@@ -21,11 +21,11 @@ class Solution:
         root = result = ListNode(None)
         heap = []
 
-        # 각 연결 리스트의 루트를 힙에 저장
+        # save each listnode's head in the heap
         for i in range(len(lists)):
             if lists[i]:
                 heapq.heappush(heap, (lists[i].val, i, lists[i]))
-        # 힙 추출 이후 다음 노드는 다시 저장
+        # after doing pop from heap, save next node in the heap.
         while heap:
             node = heapq.heappop(heap)
             idx = node[1]
