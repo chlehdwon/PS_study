fb2040f6ca79f554ec2a2f6f993da6dd0b839314
chlehdwon <cookie000215@gmail.com>
2021-01-26 23:48:28 +0000

    2021-01-27

--- 783.MinimumDistanceBetweenBSTNodes.py
+++ 783.MinimumDistanceBetweenBSTNodes.py
@@ -4,6 +4,8 @@ minimum difference between the values of any two different nodes in the
 tree.
 """
 
+import sys
+
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -14,5 +16,61 @@ class TreeNode:
 
 
 class Solution:
+    prev: int = float('-inf')
+    diff: int = float('inf')
+
     def minDiffInBST(self, root: TreeNode) -> int:
-        
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            if self.prev != float('-inf'):
+                self.diff = min(self.diff, node.val - self.prev)
+            self.prev = node.val
+            if node.right:
+                dfs(node.right)
+            return self.diff
+
+        dfs(root)
+        return self.diff
+
+
+class Solution2:
+    prev = -sys.maxsize
+    result = sys.maxsize
+
+    # 재귀 구조 중위 순회 비교 결과
+    def minDiffInBST(self, root: TreeNode) -> int:
+        if root.left:
+            self.minDiffInBST(root.left)
+
+        self.result = min(self.result, root.val - self.prev)
+        self.prev = root.val
+
+        if root.right:
+            self.minDiffInBST(root.right)
+
+        return self.result
+
+
+class Solution3:
+    def minDiffInBST(self, root: TreeNode) -> int:
+        prev = -sys.maxsize
+        result = sys.maxsize
+
+        stack = []
+        node = root
+
+        # 반복 구조 중위 순회 비교 결과
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+
+            result = min(result, node.val - prev)
+            prev = node.val
+
+            node = node.right
+
+        return result
