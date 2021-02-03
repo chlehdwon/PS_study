ca4ab7fe2364609e1b06935a3a814862f79ff4aa
chlehdwon <cookie000215@gmail.com>
2021-02-03 14:54:07 +0000

    21-02-03

--- 136.SingleNumber.py
+++ 136.SingleNumber.py
@@ -1,8 +1,11 @@
-# Given a non-empty array of integers nums, every element appears twice
-# except for one. Find that single one.
+"""
+Given a non-empty array of integers nums, every element appears twice
+except for one. Find that single one.
 
-# Follow up: Could you implement a solution
-# with a linear runtime complexity and without using extra memory?
+Follow up: Could you implement a solution with a linear runtime
+complexity and without using extra memory?
+"""
+
 
 from collections import defaultdict
 
