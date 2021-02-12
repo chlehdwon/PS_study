5e63950c28c491adf15d571b36df3a808108c4b7
chlehdwon <cookie000215@gmail.com>
2021-02-12 02:58:18 +0000

    21-02-12

--- 241.DifferentWaystoAddParentheses.py
+++ 241.DifferentWaystoAddParentheses.py
@@ -10,4 +10,41 @@ from typing import List
 
 class Solution:
     def diffWaysToCompute(self, input: str) -> List[int]:
-        return True
+        if input.isdigit():
+            return [int(input)]
+        res = []
+        for i in range(len(input)):
+            if input[i] in "-+*":
+                left = self.diffWaysToCompute(input[:i])
+                right = self.diffWaysToCompute(input[i+1:])
+                for l in left:
+                    for r in right:
+                        if input[i] == '+':
+                            res.append(l+r)
+                        elif input[i] == '-':
+                            res.append(l-r)
+                        elif input[i] == '*':
+                            res.append(l*r)
+        return res
+
+
+class Solution2:
+    def diffWaysToCompute(self, input: str) -> List[int]:
+        def compute(left, right, op):
+            results = []
+            for l in left:
+                for r in right:
+                    results.append(eval(str(l) + op + str(r)))
+            return results
+
+        if input.isdigit():
+            return [int(input)]
+
+        results = []
+        for index, value in enumerate(input):
+            if value in "-+*":
+                left = self.diffWaysToCompute(input[:index])
+                right = self.diffWaysToCompute(input[index + 1:])
+
+                results.extend(compute(left, right, value))
+        return results
