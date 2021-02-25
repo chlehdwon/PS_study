1958d7bde4843608c76bfd262f5b28045891004a
chlehdwon <cookie000215@gmail.com>
2021-02-25 14:52:14 +0000

    21-02-25

---
+++ 11718.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+ 
+int main(){ 
+    std::string str;
+    while (true){
+        getline(std::cin, str);
+        if (str=="")
+            break;
+        std::cout << str << std::endl;
+    } 
+    return 0;
+}
