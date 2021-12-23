fe735a1f83e8dc9d674ecf474e11615956870a8d
chlehdwon <cookie000215@gmail.com>
2021-12-23 22:07:28 +0900

    21-12-23

--- LeetCode/64.MinimumPathSum.py
+++ LeetCode/64.MinimumPathSum.py
@@ -3,7 +3,46 @@
 from typing import List
 
 class Solution:
+    cache = []
+
+    # top-down answer
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
+        self.cache = [[[-1]*n] for _ in range(m)]
+        
+        return self.dp(0, 0, grid)
+
+    # bottom-up answer
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i==0 and j>0:
+                    grid[i][j]=grid[i][j]+grid[i][j-1]
+                elif j==0 and i>0:
+                    grid[i][j]=grid[i][j]+grid[i-1][j]
+                elif i>0 and j>0:
+                    grid[i][j]=grid[i][j]+min(grid[i-1][j],grid[i][j-1])
+        
+        return grid[-1][-1]
     
-    def dp()
+    def dp(self, y: int, x: int, grid: List[List[int]]) -> int:
+        print(self.cache, y, x)
+        if self.cache[y][x] != -1:
+            return self.cache[y][x]
+        else:
+            m, n = len(grid), len(grid[0])
+            if y==m-1 and x==n-1:
+                self.cache[-1][-1] = grid[-1][-1]
+                return self.cache[-1][-1]
+            elif y==m-1:
+                self.cache[y][x] = grid[y][x] + self.dp(y,x+1,grid)
+                return self.cache[y][x]
+            elif x==n-1:
+                self.cache[y][x] = grid[y][x] + self.dp(y+1,x,grid)
+                return self.cache[y][x]
+            else:
+                self.cache[y][x] = grid[y][x] + min(self.dp(y+1,x,grid), self.dp(y,x+1,grid))
+                return self.cache[y][x]
+
+a = Solution()
+print(a.minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
